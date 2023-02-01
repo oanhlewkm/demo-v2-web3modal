74a6e73eaa2338b6d4396f34be1d99fdce0a0cb8
oanhlewkm <oanhle@wakumo.vn>
2023-02-01 17:01:09 +0700

    finish connect v2 walletconnect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,87 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
 
+import { Web3Modal } from "@web3modal/react";
+
+import { useEffect, useState } from 'react';
+
+import { WagmiConfig, createClient, configureChains,  } from 'wagmi'
+import { goerli, polygonMumbai } from 'wagmi/chains'
+ 
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+ 
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+// 1. Get projectID at https://cloud.walletconnect.com
+if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
+  throw new Error('You need to provide NEXT_PUBLIC_PROJECT_ID env variable')
+}
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+// 2. Configure wagmi client
+const chains = [goerli]
+const { provider, webSocketProvider } = configureChains(chains, [walletConnectProvider({ projectId })])
+export const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: [
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+        projectId,
+        version: "2"
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+  // connectors: modalConnectors({
+  //   appName: 'web3Modal',
+  //   chains,
+  //   projectId,
+  //   version: "2"
+  // }),
+  // provider
+})
+
+// 3. Configure modal ethereum client
+export const ethereumClient = new EthereumClient(
+  wagmiClient, chains
+  )
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true)
+  }, []);
+
+  return ready ? (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+
+      <Web3Modal
+        ethereumClient={ethereumClient}
+        projectId={projectId}
+      />
+    </>
+  ) : null;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Web3Button } from '@web3modal/react'
+import { useAccount, useConnect, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { address, connector } = useAccount()
+  const { data: ensAvatar } = useEnsAvatar({ address })
+  const { data: ensName } = useEnsName({ address })
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect()
+  const { disconnect } = useDisconnect()
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    if (address) setIsConnected(true);
+    else if (isConnected) setIsConnected(false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [address]);
+
   return (
     <>
       <Head>
@@ -14,110 +31,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <div>
+        {isConnected ? (
+           <div>
+            Connected to {address}
+            <button onClick={() => disconnect()}>Disconnect</button>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        ) : (
+          <>
+            {connectors.map((connector) => (
+              <button
+                disabled={!connector.ready}
+                key={connector.id}
+                onClick={() => connect({ connector })}
+              >
+                {connector.name}
+                {!connector.ready && ' (unsupported)'}
+                {isLoading &&
+                  connector.id === pendingConnector?.id &&
+                  ' (connecting)'}
+              </button>
+            ))}
+          </>
+        )}
+      
+ 
+      {error && <div>{error.message}</div>}
+    </div>
     </>
   )
 }
